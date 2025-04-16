e11b9d5300d14f5cedd4f6e1226f55bee6ca43d1
Somasundaram M <msomasundaram93@gmail.com>
2025-04-16 19:47:18 +0530

    Remove unused layouts and utility functions for `ships` and `thoughts`
    Deleted `ships-layout` and `thoughts-layout` components along with their associated utilities and references. Updated the exports in `index.ts` to reflect the removal. This streamlines codebase maintenance by eliminating unused modules.

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,5 +1,4 @@
 export { default as UsesLayout } from "./uses-layout.astro";
 export { default as WritingLayout } from "./writing-layout.astro";
-export { default as ShipsLayout } from "./ships-layout.astro";
-export { default as ThoughtsLayout } from "./thoughts-layout.astro";
+export { default as ThinkInCodeLayout } from "./think-in-code-layout.astro";
 export { default as RootLayout } from "./root-layout.astro";
