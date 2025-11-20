1b5cc98145f3898bfec3ae5a2c383c30a24e9d30
Somasundaram M <somasundaram.mahesh@jiostar.com>
2025-11-20 21:06:54 +0530

    Refactor import statements for consistency

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
+import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 import { SITE_DESCRIPTION, SITE_TITLE } from "@data/index";
-import { getCollection } from "astro:content";
 
 export async function GET(context) {
 	const [posts, uses, thinkInCodeItems] = await Promise.all([
